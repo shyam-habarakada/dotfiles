f15813c8dbf670b6cd3af9f5e10677699cb35d0f
shyam-habarakada <shyam.habarakada@gmail.com>
2019-01-11 10:00:23 -0800

    Add keepingyouawake install
    Caffeine replacement

--- install-desktop-apps.sh
+++ install-desktop-apps.sh
@@ -3,3 +3,4 @@ brew cask install --appdir="/Applications" textmate
 brew cask install --appdir="/Applications" fantastical
 brew cask install --appdir="/Applications" google-chrome
 brew cask install --appdir="/Applications" 1password
+brew cask install --appdir="/Applications" keepingyouawake
